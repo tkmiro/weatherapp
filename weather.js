78e40ad4b17375564df8e677317bf86bedc64ed6
tkmiro <42407639+tkmiro@users.noreply.github.com>
2019-02-12 08:04:27 +0100

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,37 @@
+new Vue({
+    el: "#app",
+    data: {
+        userInput: "",
+        weather: null,
+        temperature: null,
+        icon: null,
+        humidity: null,
+        pressure: null,
+        wind: null,
+        sunrise: null,
+        sunset: null,
+        icon: null
+        
+        
+    },
+    methods: {
+        apiRequest: function(){
+            axios.get("http://api.openweathermap.org/data/2.5/weather?q=" + this.userInput + "&units=metric&appid=d9ce0e13783eb6a02995fab153ae9dc8")
+            .then((resp) => {
+                this.weather = resp.data.weather[0].description;
+                this.temperature = Math.round(resp.data.main.temp) + " C";
+                this.humidity = resp.data.main.humidity + " %";
+                this.pressure = resp.data.main.pressure + " hPa";
+                this.wind = resp.data.wind.speed + " m/s";
+                this.sunrise = new Date(resp.data.sys.sunrise * 1e3).toISOString().slice(-13, -5);
+                this.sunset = new Date(resp.data.sys.sunset * 1e3).toISOString().slice(-13, -5);
+                this.icon = "http://openweathermap.org/img/w/" + resp.data.weather[0].icon + ".png"
+            })
+            
+    }
+    }  
+    
+    
+    
+
+})
